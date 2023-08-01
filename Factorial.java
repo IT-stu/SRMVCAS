1b0d71cfdfad2856c72be61f130c970dbc9caceb
Mukesh jayabal <mukesh.it2005@gmail.com>
2023-08-01 21:37:40 +0530

    Modified

--- Factorial.java
+++ Factorial.java
@@ -2,9 +2,10 @@ class Factorial
 {  
  public static void main(String args[])
  {  
-  int i,fact=1;  
+  int i;
+  int fact=10;  
   int number =10;
-  for(i=1;i<=number;i++)
+  for(i = 10;i>1;i--)
   	{    
       		fact=fact*i;    
   		
